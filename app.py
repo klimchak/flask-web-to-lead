55fbc9b99301e01dc6311c7b35a6f4aea47967aa
klim <shpektras@gmail.com>
2021-05-12 14:31:32 +0300

    uncommenting func SF

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request, abort, redirect, url_for, fla
 from simple_salesforce import Salesforce
 from flask_recaptcha import ReCaptcha
 
-# sf = Salesforce(username='webtoleadtask8@gmail.com', password='JfCx5UY55YHSxx7',
-#                 security_token='pPbkhqsnhYo4j0YuPfrUBbwn')
+sf = Salesforce(username='webtoleadtask8@gmail.com', password='JfCx5UY55YHSxx7',
+                security_token='pPbkhqsnhYo4j0YuPfrUBbwn')
 
 app = Flask(__name__)
 recaptcha = ReCaptcha(app=app)
@@ -35,35 +35,32 @@ def add_new_data():
     if request.method == 'POST':
         if recaptcha.verify():
             if 'add_shipping_address' in request.form:
-                data = True
-                # data = sf.Lead.create({'Company': request.form['company_name'],
-                #                        'Website': request.form['company_site'],
-                #                        'FirstName': request.form['first_name'],
-                #                        'LastName': request.form['last_name'],
-                #                        'Birthdate__c': request.form['birthday'],
-                #                        'Email': request.form['email'],
-                #                        'Country': request.form['billing_country'],
-                #                        'State': request.form['billing_state'],
-                #                        'PostalCode': request.form['billing_zip'],
-                #                        'City': request.form['billing_city'],
-                #                        'Street': request.form['billing_street'],
-                #                        'Add_shipping_address__c': True})
+                data = sf.Lead.create({'Company': request.form['company_name'],
+                                       'Website': request.form['company_site'],
+                                       'FirstName': request.form['first_name'],
+                                       'LastName': request.form['last_name'],
+                                       'Birthdate__c': request.form['birthday'],
+                                       'Email': request.form['email'],
+                                       'Country': request.form['billing_country'],
+                                       'State': request.form['billing_state'],
+                                       'PostalCode': request.form['billing_zip'],
+                                       'City': request.form['billing_city'],
+                                       'Street': request.form['billing_street'],
+                                       'Add_shipping_address__c': True})
             else:
-                data = False
-                # data = sf.Lead.create({'Company': request.form['company_name'],
-                #                        'Website': request.form['company_site'],
-                #                        'FirstName': request.form['first_name'],
-                #                        'LastName': request.form['last_name'],
-                #                        'Birthdate__c': request.form['birthday'],
-                #                        'Email': request.form['email'],
-                #                        'Country': request.form['billing_country'],
-                #                        'State': request.form['billing_state'],
-                #                        'PostalCode': request.form['billing_zip'],
-                #                        'City': request.form['billing_city'],
-                #                        'Street': request.form['billing_street'],
-                #                        'Add_shipping_address__c': False})
-            # if 'success' in data and data['success'] is True:
-            if data is True:
+                data = sf.Lead.create({'Company': request.form['company_name'],
+                                       'Website': request.form['company_site'],
+                                       'FirstName': request.form['first_name'],
+                                       'LastName': request.form['last_name'],
+                                       'Birthdate__c': request.form['birthday'],
+                                       'Email': request.form['email'],
+                                       'Country': request.form['billing_country'],
+                                       'State': request.form['billing_state'],
+                                       'PostalCode': request.form['billing_zip'],
+                                       'City': request.form['billing_city'],
+                                       'Street': request.form['billing_street'],
+                                       'Add_shipping_address__c': False})
+            if 'success' in data and data['success'] is True:
                 return render_template('success.html', data=request.form)
             else:
                 return render_template('hello.html', data=request.form)
